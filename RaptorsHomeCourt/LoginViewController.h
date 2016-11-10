8f9bc66e4ba15264debd365df99b84a254b3af8e
Xcode User <xcode@fasts-macbook-pro-6.local>
2016-11-10 11:18:06 -0500

    Created Initial Project Files

---
+++ RaptorsHomeCourt/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  RaptorsHomeCourt
+//
+//  Created by Xcode User on 2016-11-10.
+//  Copyright © 2016 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ RaptorsHomeCourt/SeatSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  SeatSelectViewController.h
+//  RaptorsHomeCourt
+//
+//  Created by Xcode User on 2016-11-10.
+//  Copyright © 2016 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeatSelectViewController : UIViewController
+
+@end
