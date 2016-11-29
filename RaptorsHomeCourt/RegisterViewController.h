35489586f7efcb5fddc927d166755bc5a06c3df3
dickgumshoes <vindradraconia@gmail.com>
2016-11-29 10:44:13 -0500

    added RaptorsLogin.db, added RegisterVC, changed LoginVC, changed NavBars

---
+++ RaptorsHomeCourt/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  RaptorsHomeCourt
+//
+//  Created by Xcode User on 2016-11-28.
+//  Copyright © 2016 Xcode User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
